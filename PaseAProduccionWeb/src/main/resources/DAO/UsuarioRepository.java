f84c498e1c6d20721706fd73ec8d791d675caa28
jmoscoso <jmoscoso@DESARROLLO07>
2015-06-15 10:21:45 -0500

    DAO user

---
+++ PaseAProduccionWeb/src/main/resources/DAO/UsuarioRepository.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import bean.PpUsuarios;
+import java.util.List;
+import org.hibernate.Session;
+
+
+/**
+ *
+ * @author jmoscoso
+ */
+public class UsuarioRepository implements BasicRepository<PpUsuarios, Integer>{
+
+    @Override
+    public void add(PpUsuarios e) {
+        
+            Session session = newHibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(e);
+            session.getTransaction().commit();
+    }
+
+    @Override
+    public void update(PpUsuarios e) {
+         Session session = newHibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(e);
+            session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(Integer id) {
+      Session session = newHibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(find(id));
+            session.getTransaction().commit();
+    
+    }
+
+    @Override
+    public PpUsuarios find(Integer id) {
+        Session session = newHibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            PpUsuarios usuario =   (PpUsuarios)session.get(PpUsuarios.class, id);
+            session.getTransaction().commit();
+    
+            return usuario;
+    }
+
+    @Override
+    public List<PpUsuarios> listAll() {
+    
+        Session session = newHibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            List<PpUsuarios> LstUsuario = session.createQuery("from PpUsuarios").list();
+        session.getTransaction().commit();
+        
+        return LstUsuario;
+    }
+    
+    
+    
+}
